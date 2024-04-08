62f42445c263a6d28d5a3a709d12303503ec4b2e
Harshita Sharma <sharmaharshita711@gmail.com>
2024-04-08 23:26:07 +0530

    chnages

---
+++ inbuilt.js
@@ -0,0 +1,11 @@
+const user = {
+    name: "John",
+    age: 24,
+    dept: "HR"
+}
+
+const values = Object.values(user);
+const keys = Object.keys(user);
+const entries = Object.entries(user);
+console.log(values, keys);
+console.log(entries);
